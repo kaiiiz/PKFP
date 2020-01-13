f534b553250f8eda2ea344d9fe08ebb4cc7da56a
kaiiiz <at881005@gmail.com>
2020-01-13 14:47:06 +0800

    implement buffer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "mbed.h"
+#include "keymap.h"
 #include <string>
+#include <queue>
+#include <unordered_map>
 
 DigitalIn btn(PC_13);
 DigitalIn fp(PC_5); // foot pedal
+DigitalOut led(LED1);
 Serial pc(USBTX, USBRX);
 Serial bt(PC_1, PC_0); // tx, rx
 
@@ -29,6 +33,17 @@ void hid_mode() {
 
 class Shell {
    public:
+    std::unordered_map<string, int> keymap;
+    std::queue<int> buffer;
+    int buffer_max = 3;
+
+    Shell() {
+        init_keymap(keymap);
+        buffer.push(keymap["a"]);
+        buffer.push(keymap["b"]);
+        buffer.push(keymap["Esc"]);
+    }
+
     void main() {
         char rxBuf[128];
         while (bt.scanf("%s", rxBuf)) {
@@ -39,7 +54,18 @@ class Shell {
                 hid_mode();
                 break;
             }
+            else if (!strcmp(rxBuf, "help")) {
+                help();
+            }
         }
+    }
+    
+   private:
+    void help() {
+        bt.printf("usage: \r\n");
+        bt.printf("\r\n");
+        bt.printf("show buffer: show key buffer\r\n");
+        bt.printf("hid: reset profile to hid\r\n");
     }
 };
 
@@ -64,35 +90,64 @@ bool btnIsPress() {
     return false;
 }
 
-void fpHandler() {
+void fpHandler(std::queue<int>& buffer) {
+    bool long_press = false;
     if (fp == 1) { // debounce
         // press
         int counter = 0xffff;
         while (counter) {
-            if (fp == 1)  counter >>= 1;
-            else return;
+            if (fp == 1) {
+                counter >>= 1;
+            }
+            else {
+                return;
+            }
             wait_us(1000);
+        }
+        // long press check
+        counter = 0xfffff;
+        while (counter) {
+            if (fp == 0) {
+                long_press = false;
+                break;
+            }
+            else {
+                counter >>= 1;
+                long_press = true;
+            }
+            wait_us(100000);
         }
         // release
         counter = 0xffff;
         while (counter) {
-            if (fp == 0) counter >>= 1;
-            else counter = 0xffff;
+            if (fp == 0) {
+                counter >>= 1;
+            }
+            else {
+                counter = 0xffff;
+            }
             wait_us(1000);
         }
-        bt.putc('a');
+
+        if (long_press) {
+            buffer.push(buffer.front());
+            buffer.pop();
+            led = 1;
+        }
+        else {
+            bt.putc(buffer.front());
+            led = 0;
+        }
     }
 }
 
 int main() {
-    Shell shell;
-    BtProfile profile = HID;
-    hid_mode();
-
-    std::string input;
-    
     pc.baud(115200);
     bt.baud(115200);
+
+    Shell shell;
+    BtProfile profile = SPP;
+    serial_mode();
 
     while (1) {
         if (profile == HID) {
@@ -100,7 +155,7 @@ int main() {
                 serial_mode();
                 profile = SPP;
             }
-            fpHandler();
+            fpHandler(shell.buffer);
         }
         else if (profile == SPP) {
             shell.main();
